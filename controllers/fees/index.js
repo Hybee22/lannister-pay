93e40c1f1d4ec48c2ffec05ccf1f9bb063ce1561
Ibrahim Adekunle <adefemi101@gmail.com>
2022-03-17 15:56:34 +0100

    removed config for using fs

--- controllers/fees/index.js
+++ controllers/fees/index.js
@@ -1,4 +1,3 @@
-import fs from "fs";
 import redisKeys from "../../redis/key-gen.js";
 import { errorResMsg, successResMsg } from "../../utilities/response.js";
 import parseFCS from "../../utilities/parseFCS.js";
@@ -16,11 +15,6 @@ class FeesController {
       const data = parseFCS(FeeConfigurationSpec);
       // Generate a key to save the stringified configuration objects
       const keyId = redisKeys.getHashKey(`config`);
-
-      // Save to File
-      fs.writeFile("fee-config.json", JSON.stringify(data), (err) => {
-        if (err) logger.error(err);
-      });
 
       // Set Config Data in Redis store
       await cache.set(keyId, data);
@@ -48,65 +42,46 @@ class FeesController {
 
       const keyId = redisKeys.getHashKey(`config`);
 
-      let configArr;
+      let configArr = await cache.get(keyId);
 
-      let path = "fee-config.json";
-
-      if (fs.existsSync(path)) {
-        return fs.readFile("fee-config.json", async (err, data) => {
-          if (err) {
-            configArr = await cache.get(keyId);
-            if (!configArr) {
-              return errorResMsg(res, 400, {
-                message: "No fee configurations found",
-              });
-            }
-          }
-          configArr = JSON.parse(data);
-
-          if (!configArr) {
-            return errorResMsg(res, 400, {
-              message: "No fee configurations found",
-            });
-          }
-
-          let LOCALE;
-          if (CurrencyCountry === Country) {
-            LOCALE = "LOCL";
-          } else {
-            LOCALE = "INTL";
-          }
-
-          const applied = appliedFeeValue(configArr, Amount, {
-            TYPE: Type,
-            BRAND: Brand,
-            LOCALE: LOCALE,
-            CURRENCY: Currency,
-          });
-
-          if (applied?.message?.length > 0) {
-            return errorResMsg(res, 400, { Error: applied?.message });
-          }
-
-          let ChargeAmount;
-          if (BearsFee === true) {
-            ChargeAmount = Amount + applied?.appliedFee;
-          } else {
-            ChargeAmount = Amount;
-          }
-
-          const SettlementAmount = ChargeAmount - applied?.appliedFee;
-
-          return successResMsg(res, 200, {
-            AppliedFeeId: applied?.feeId,
-            AppliedFeeIdValue: Math.round(applied?.appliedFee),
-            ChargeAmount,
-            SettlementAmount,
-          });
+      if (!configArr) {
+        return errorResMsg(res, 400, {
+          message: "No fee configurations found",
         });
       }
-      return errorResMsg(res, 400, {
-        message: "No fee configurations found",
+
+      let LOCALE;
+      if (CurrencyCountry === Country) {
+        LOCALE = "LOCL";
+      } else {
+        LOCALE = "INTL";
+      }
+
+      const applied = appliedFeeValue(configArr, Amount, {
+        TYPE: Type,
+        BRAND: Brand,
+        LOCALE: LOCALE,
+        CURRENCY: Currency,
+      });
+
+      if (applied?.message?.length > 0) {
+        return errorResMsg(res, 400, { Error: applied?.message });
+      }
+
+      let ChargeAmount;
+      if (BearsFee === true) {
+        ChargeAmount = Amount + applied?.appliedFee;
+      } else {
+        ChargeAmount = Amount;
+      }
+
+      const SettlementAmount = ChargeAmount - applied?.appliedFee;
+
+      return successResMsg(res, 200, {
+        AppliedFeeId: applied?.feeId,
+        AppliedFeeIdValue: Math.round(applied?.appliedFee),
+        ChargeAmount,
+        SettlementAmount,
       });
     } catch (error) {
       console.log(error);
